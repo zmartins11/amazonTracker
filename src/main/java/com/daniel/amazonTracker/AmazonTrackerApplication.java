16678e3236d63cfcc4a29912b0a5d81f5413e4c6
zmartins11 <daniel98bm@gmail.com>
2023-09-20 23:35:35 +0100

    first commit

---
+++ src/main/java/com/daniel/amazonTracker/AmazonTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.amazonTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/amazonTracker/AmazonTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.amazonTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
